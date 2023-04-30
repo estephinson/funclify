f550134b9602d02173cb19a6093ed5b5349c3218
Ed Stephinson <edward.stephinson@gmail.com>
2023-04-30 20:45:48 +0100

    Update testing harness

--- src/testing.ts
+++ src/testing.ts
@@ -1,5 +1,9 @@
 import { Api } from './index';
-import { HandlerEvent, HandlerResponse } from '@netlify/functions';
+import {
+  HandlerContext,
+  HandlerEvent,
+  HandlerResponse,
+} from '@netlify/functions';
 
 export interface TestRequest<TBody = unknown> {
   method: string;
@@ -9,6 +13,7 @@ export interface TestRequest<TBody = unknown> {
   params?: Record<string, string>;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export class ApiTestHarness<TApi extends Api<any>> {
   private api: TApi;
   constructor(api: TApi) {
@@ -16,47 +21,82 @@ export class ApiTestHarness<TApi extends Api<any>> {
   }
 
   async handleRequest(request: TestRequest): Promise<HandlerResponse> {
+    const params = request.params ?? {};
+
+    if (request.url.includes('?')) {
+      const [url, queryString] = request.url.split('?');
+      const query = new URLSearchParams(queryString);
+
+      for (const [key, value] of query.entries()) {
+        params[key] = value;
+      }
+
+      request.url = url;
+    }
+
     return this.api.baseHandler(
       {
         httpMethod: request.method,
         path: request.url,
         headers: request.headers,
         body: request.body,
-        queryStringParameters: request.params ?? {},
+        queryStringParameters: params,
       } as HandlerEvent,
-      {} as any
+      {} as HandlerContext
     );
   }
 
-  async get(url: string) {
+  async get(url: string, headers: Record<string, string> = {}) {
     return this.handleRequest({
       method: 'GET',
       url,
-      headers: {},
+      headers,
       body: null,
     });
   }
-  async post(url: string, body: any) {
+  async post<TBody>(
+    url: string,
+    body: TBody,
+    headers: Record<string, string> = {}
+  ) {
     return this.handleRequest({
       method: 'POST',
       url,
-      headers: {},
+      headers,
       body,
     });
   }
-  async put(url: string, body: any) {
+  async put<TBody>(
+    url: string,
+    body: TBody,
+    headers: Record<string, string> = {}
+  ) {
     return this.handleRequest({
       method: 'PUT',
       url,
-      headers: {},
+      headers,
       body,
     });
   }
-  async delete(url: string) {
+
+  async patch<TBody>(
+    url: string,
+    body: TBody,
+    headers: Record<string, string> = {}
+  ) {
+    return this.handleRequest({
+      method: 'PATCH',
+      url,
+      headers,
+      body,
+    });
+  }
+
+  async delete(url: string, headers: Record<string, string> = {}) {
     return this.handleRequest({
       method: 'DELETE',
       url,
-      headers: {},
+      headers,
       body: null,
     });
   }
